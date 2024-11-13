a79c72871bb409a48cc6e0d7bb718c2386fc25fb
IsraLop <usraellopezalvarado@gmail.com>
2024-11-13 00:55:59 -0600

    cambio en registro pedido

--- PF_BD/pf_bd/src/main/webapp/menu.js
+++ PF_BD/pf_bd/src/main/webapp/menu.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Redireccionar a la página de insertar
+    // Redireccionar a la página de insertar cliente y pedido
     document.getElementById('btn-insertar').addEventListener('click', function() {
-        window.location.href = 'insertarCliente.jsp';
+        window.location.href = 'insertarClienteYPedido.jsp';  // Asegúrate de que este sea el nombre correcto
     });
 
     // Redireccionar a la página de mantenimiento de cliente
     document.getElementById('btn-mantenimiento').addEventListener('click', function() {
-        window.location.href = 'ConsultaC.jsp';
+        window.location.href = 'consultarC.jsp';  // Asegúrate de que este sea el nombre correcto
     });
 
     // Redireccionar a la página de consulta de pedido
     document.getElementById('btn-mantenimientoP').addEventListener('click', function() {
-        window.location.href = 'ConsultaP.jsp';
+        window.location.href = 'consultarPedido.jsp';  // Asegúrate de que este sea el nombre correcto
     });
 
-    // Redireccionar a la página de consulta de cliente y pedido
+    // Redireccionar a la página de consulta general
     document.getElementById('btn-Consulta').addEventListener('click', function() {
-        window.location.href = 'ConsultaCP.jsp';
+        window.location.href = 'consultaGeneral.jsp';  // Asegúrate de que este sea el nombre correcto
     });
-    
+
     // Redireccionar a la página de inicio de sesión
     document.getElementById('btn-salir').addEventListener('click', function() {
-        window.location.href = 'index.jsp';
+        window.location.href = 'index.jsp';  // Asegúrate de que este sea el nombre correcto
     });
 });
